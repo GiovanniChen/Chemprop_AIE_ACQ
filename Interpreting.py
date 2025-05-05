8e66c60641db759cd35ea8f6f38f947d46352fe0
GiovanniChen <134799007+GiovanniChen@users.noreply.github.com>
2025-05-05 08:18:15 +0800

    Update Interpreting.py

--- Interpreting.py
+++ Interpreting.py
@@ -4,12 +4,12 @@ def main():
     arguments = [
         # '--num_workers', '0',
         # 并行加载工作线程数
-        '--data_path', './data/AIE_ACQ/AIEACQ_interpret.csv',
+        '--data_path', 'AIEACQ_interpret.csv',
         '--features_generator', 'rdkit_2d_normalized',
         # 'morgan', 'morgan_count', 'rdkit_2d', 'rdkit_2d_normalized'
         '--no_features_scaling',
         # When using rdkit_2d_normalized features, --no_features_scaling must be specified.
-        '--checkpoint_dir', '20240503_checkpoints_RDKit_best_forinterpret',
+        '--checkpoint_dir', 'checkpoints_RDKit_forinterpret',
 
         '--max_atoms', '50',
         '--min_atoms', '15',
